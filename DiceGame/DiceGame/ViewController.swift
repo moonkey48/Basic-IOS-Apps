b3a1353b3d6a3699b11baff5eb2161d87ac6c87c
= <moonkey48@icloud.com>
2023-02-03 12:29:45 +0900

    [Dice Game] make dice app

--- DiceGame/DiceGame/ViewController.swift
+++ DiceGame/DiceGame/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+//        firstImageView.image = diceArray[Int.random(in: 0...5)]
+//        secondImageView.image = diceArray[Int.random(in: 0...5)]
+        firstImageView.image = diceArray.randomElement()
+        secondImageView.image = diceArray.randomElement()
+    }
 }
 
