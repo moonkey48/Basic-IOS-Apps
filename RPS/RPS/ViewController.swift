bb2cb23bd59ac3883d98ea7c2acf7521b7c4297c
= <moonkey48@icloud.com>
2023-02-03 13:25:24 +0900

    [RPS] connect layout with Controller

--- RPS/RPS/ViewController.swift
+++ RPS/RPS/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
+    @IBOutlet weak var computerImageView: UIImageView!
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    
+    @IBOutlet weak var computerLabel: UILabel!
+    @IBOutlet weak var userLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func rpsButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
