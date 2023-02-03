a201705c28f443ac8cf919bc71cc8164ce4d0569
= <moonkey48@icloud.com>
2023-02-03 13:02:31 +0900

    [RPS] make RPS game layout

---
+++ RPS/RPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by moonkey on 2023/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
