b91639837edea81385ac86c02a20e9a22088eb70
= <moonkey48@icloud.com>
2023-02-04 13:34:04 +0900

    [Timer] make basic UI structure

---
+++ Timer/Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by moonkey on 2023/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
