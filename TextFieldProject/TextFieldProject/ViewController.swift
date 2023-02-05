245b91c50e5a9b255d3ba815ea3f50d539e8d2cf
= <moonkey48@icloud.com>
2023-02-05 12:48:15 +0900

    [TextFieldProject] add delegate pattern on textField

--- TextFieldProject/TextFieldProject/ViewController.swift
+++ TextFieldProject/TextFieldProject/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUp()
+        textField.delegate = self
+    }
+    func setUp(){
+        view.backgroundColor = UIColor.gray
+        textField.keyboardType = UIKeyboardType.emailAddress
+        textField.placeholder = "이메일 입력"
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("on textField")
+    }
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        return false
+    }
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(string)
+        return true
     }
 
-
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
