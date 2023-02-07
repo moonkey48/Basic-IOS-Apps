0ef49623002b13a5cb6b293204001ce1e1662c48
= <moonkey48@icloud.com>
2023-02-07 14:03:12 +0900

    [BMI] add calculate for bmi with user height and weight

--- BMI/BMI/SecondViewController.swift
+++ BMI/BMI/SecondViewController.swift
@@ -10,19 +10,29 @@ import UIKit
 class SecondViewController: UIViewController {
 
     
-    @IBOutlet weak var bmiumberLabel: UILabel!
-    
+    @IBOutlet weak var bmiNumberLabel: UILabel!
     @IBOutlet weak var adiviceLabel: UILabel!
-    
     @IBOutlet weak var backButton: UIButton!
+    
+    var bmiNumber: Double?
+    var bmiString: String?
+    var bmiColor: UIColor?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        makeUI()
     }
     private func makeUI(){
         backButton.clipsToBounds = true
         backButton.layer.cornerRadius = 5
+        
+        
+        guard let bmiNumber = bmiNumber else {return}
+        bmiNumberLabel.text = String(bmiNumber)
+        
+        //어차피 optional이라 guard가 필요 없다.
+        adiviceLabel.text = bmiString
+        bmiNumberLabel.backgroundColor = bmiColor
     }
     
     
