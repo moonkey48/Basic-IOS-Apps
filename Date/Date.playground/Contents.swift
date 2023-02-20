2807637be2e3c1fc6eb78257d482dd65f26bb06c
= <moonkey48@icloud.com>
2023-02-20 21:34:40 +0900

    [Date] add Calender use-case

--- Date/Date.playground/Contents.swift
+++ Date/Date.playground/Contents.swift
@@ -7,3 +7,14 @@ now
 now.timeIntervalSince1970
 now.timeIntervalSinceReferenceDate
 
+
+enum Weekday: Int {
+    case sunday = 1, monday, tuesday, wednesday, thursday, friday, saturday
+    
+    static var today: Weekday {
+        let weekday = Calendar.current.component(.weekday, from: Date())
+        return Weekday(rawValue: weekday)!
+    }
+}
+Weekday.today
+
